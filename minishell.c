2f63d2fc459edfa99dc436248aba799f908deeaa
n-hass <nickhassan01@icloud.com>
2023-08-06 19:59:23 +0930

    improved code comments

--- minishell.c
+++ minishell.c
@@ -6,6 +6,8 @@
    File			: minishell.c
    Compiler/System	: gcc/linux
 
+   Edited by Nicholas Hassan for the University of Adelaide Operating Systems COMP SCI 3004, Sem 2 2023
+
 ********************************************************************/
 
 #include <signal.h>
@@ -26,110 +28,45 @@ char line[NL]; /* command input buffer */
         shell prompt
  */
 void prompt(void) {
+  // I have commented out these prompts as they are not needed in the final submission of the autograder
   // fprintf(stdout, "\n msh> ");
   // fflush(stdout);
 }
 
+// a struct to hold the state of backgrounded/detached commands
 struct child_proc {
-  int pid;
-  int minishell_id;
-  char command[NL];
+  int pid; // the unix PID of the spawned forked process
+  int minishell_id; // the ID given by the shell for the command (starting at 1, ie [1], [2], etc.)
+  char command[NL]; // the command that is being run
 };
 
-static struct child_proc* detached;
-static int n_detached = 0;
-static int process_num = 1;
 
-// void sigchld_handler (int signum) {
-//   int status, pid;
-  
-//   while ((pid = waitpid(-1, &status, WNOHANG)) > 0) {
+static struct child_proc* detached; // a list of detached processes for tracking their execution
+static int n_detached = 0; // number of detached processes
+static int process_num = 1; // id number for backgrounding commands (starting at 1, ie [1], [2], etc.)
 
-//     // Check if the PID is in the list of background processes
-//     for (int i = 0; i < n_detached; i++) {
-//       if (detached[i].pid == pid) {
-//         if (WIFEXITED(status)) {
-//           printf("[%d]+ Done %s\n", detached[i].minishell_id, detached[i].command);
-//           // fflush(stdout);
-//           prompt();
-//         }
-//         // Remove the PID from the list
-//         for (int j = i; j < n_detached - 1; j++) {
-//           detached[j] = detached[j + 1];
-//         }
-//         n_detached--;
-//         process_num--;
-//         break;
-//       }
-//     }
 
-//     break;
-
-//   }
-//   // prompt();
-//   // if (pid == -1 && errno != ECHILD) {
-//   //   perror("Command failed");
-//   // }
-// }
-
-// void sigchld_handler (int signum) {
-//   int status, pid;
-
-//   for (int i = 0; i < n_detached; i++) {
-//     pid = waitpid(detached[i].pid, &status, WNOHANG);
-    
-//     if (WIFEXITED(status)) {
-//       printf("[%d]+ Done %s\n", detached[i].minishell_id, detached[i].command);
-//       // fflush(stdout);
-//       // prompt();
-//     }
-//     // Remove the PID from the list
-//     for (int j = i; j < n_detached - 1; j++) {
-//       detached[j] = detached[j + 1];
-//     }
-//     n_detached--;
-//     process_num--;
-//     break;
-    
-//   }
-
-// }
-
-// void sigchld_handler (int signum) {
-
-//   struct child_proc this_child = detached[0];
-
-//   // Remove this process from the list
-//   for (int j = 0; j < n_detached - 1; j++) {
-//     detached[j] = detached[j + 1];
-//   }
-//   n_detached--;
-  
-//   int status;
-//   int pid = waitpid(this_child.pid, &status, WNOHANG);
-  
-//   if (WIFEXITED(status)) {
-//     printf("[%d]+ Done %s\n", this_child.minishell_id, this_child.command);
-//     process_num--;
-//     pid++; pid--;
-//     // fflush(stdout);
-//     // prompt();
-//   }
-
-// }
-
+/**
+ * This signal handler is run every time a command finishes execution.
+ * The main purpose of this SIGCHLD handler is to monitor detached processes
+ * executed in the background. When a detached process finishes, the handler
+ * checks the processes that were launched in the background and reports completion
+ * for the appropriate command.
+ */
 void sigchld_handler (int signum) {
 
   int status, pid;
+
+  // this for loop iterates through the detached processes, checking their completion and reporting it when that occurs
   for (int i=0; i<n_detached; i++) {
-    pid = waitpid(detached[i].pid, &status, WNOHANG);
+    pid = waitpid(detached[i].pid, &status, WNOHANG); // wait until the detached process has finished
     if (pid == -1) {
       if (errno != ECHILD) perror("Command failed");
     } else if (pid > 0) {
       if (WIFEXITED(status)) {
         printf("[%d]+ Done                        %s\n", detached[i].minishell_id, detached[i].command);
       }
-      // Remove the PID from the list
+      // Remove this process from the list and move everything else up
       for (int j = i; j < n_detached - 1; j++) {
         detached[j] = detached[j + 1];
       }
@@ -141,7 +78,6 @@ void sigchld_handler (int signum) {
 
 }
 
-pid_t shell_pgid;
 
 int main(int argk, char *argv[], char *envp[])
 /* argk - number of arguments */
@@ -154,7 +90,7 @@ int main(int argk, char *argv[], char *envp[])
   char *v[NV];         /* array of pointers to command line tokens */
   char *sep = " \t\n"; /* command line token separators    */
   int i;               /* parse index */
-  int background;      /* indicator to run process in background */
+  int background;      /* indicator to run command in background */
   
   signal(SIGCHLD, sigchld_handler); /* Register the sigchld handler */
   detached = malloc(MAX_DETACHED * sizeof(struct child_proc)); // Allocate memory for the detached list
@@ -162,8 +98,6 @@ int main(int argk, char *argv[], char *envp[])
     perror("Failed to allocate memory for tracking detached processes");
     exit(EXIT_FAILURE);
   }
-
-  shell_pgid = getpgrp();
 
   /* prompt for and process one command line at a time  */
 
@@ -202,16 +136,16 @@ int main(int argk, char *argv[], char *envp[])
       }
       continue; // skip the rest of the command
     }
-
+    
     background = 0; // assume the process should not run in the background
-    if (v[i - 1] != NULL && strcmp(v[i - 1], "&") == 0) {
-      background = 1; // If the last token is "&", enable background mode
+    if (v[i - 1] != NULL && strcmp(v[i - 1], "&") == 0) { // If the last token is "&", enable background mode
+      background = 1;
       v[i - 1] = NULL; // remove the trailing & from the command
     }
 
-    if (n_detached == MAX_DETACHED) { // If the detached list is full
+    if (n_detached == MAX_DETACHED) { // If the list of detached commands is full, report an error and skip it
       printf("Max detached processes reached\n");
-      continue; // Skip the command
+      continue;
     }
 
     /* fork a child process to exec the command in v[0] */
@@ -232,30 +166,30 @@ int main(int argk, char *argv[], char *envp[])
     default: /* code executed only by parent process */
     {
       if (background) { // If background mode is enabled
-        char full_cmd[NL] = ""; // Create a charray to hold the command
+        char full_cmd[NL] = ""; // Create a charray to hold the full command
         for(int k=0; k<i-2; k++) {
           strcat(full_cmd, v[k]); // Concatenate the command tokens into a single string
           strcat(full_cmd, " ");
         }
         strcat(full_cmd, v[i-2]); // Concatenate the last token
 
-        struct child_proc new_child = {frkRtnVal, process_num, ""}; // Create a new child process
-        strcpy(new_child.command, full_cmd); // Copy the command into the child process
+        struct child_proc new_child = {frkRtnVal, process_num, ""}; // Create a new child process state container
+        strcpy(new_child.command, full_cmd);
 
-        detached[n_detached] = new_child; // Add the PID to the detached list
+        detached[n_detached] = new_child; // Add the new child process to the list of backgrounded commands being executed
         printf("[%d] %d\n", process_num, frkRtnVal);
         fflush(stdout);
         
         process_num++;
         n_detached++;
 
-        // Bring the shell back into the foreground
+        // Bring the shell back into the foreground - not needed
         // tcsetpgrp(STDIN_FILENO, shell_pgid);
 
       } else {
         wpid = wait(0); // wait for the child process to finish like normal (in foreground)
         fflush(stdout);
-        if (wpid == -1) {
+        if (wpid == -1) { // if the child process emits a signal, but the signal is not an ECHILD error
           if (errno != ECHILD) perror("Command failed");
         } else {
           // printf("%s done\n", v[0]);
@@ -266,5 +200,5 @@ int main(int argk, char *argv[], char *envp[])
     } /* switch */
   }   /* while */
 
-  free(detached); // Free the detached list
+  free(detached); // Free the memory for list of detached command state
 } /* main */
