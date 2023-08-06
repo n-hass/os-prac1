df40f4b3704d50a0780bb9b8170116877b09c447
n-hass <nickhassan01@icloud.com>
2023-08-06 20:02:05 +0930

    improve code comments for even.c

--- even.c
+++ even.c
@@ -14,22 +14,22 @@ void hup_handler(int signum) {
 
 int main(int argc, char *argv[]) {
   
-  signal(SIGINT, int_handler);
-  signal(SIGHUP, hup_handler);
+  signal(SIGINT, int_handler); // register the SIGINT handler
+  signal(SIGHUP, hup_handler); // register the SIGHUP handler
 
   int n;
   if (argc > 1) {
-    n = atoi(argv[1]);
+    n = atoi(argv[1]); // parse the first command line argument
   }
   
   int j = 0, i = 0;
-  while (j<n) {
-    if (i%2 == 0) {
-      printf("%d\n", i);
-      j++;
-      i+=2;
-      sleep(5);
-    }
+  while (j<n) { // print i n times with a five-second gap
+    
+    printf("%d\n", i);
+    j++;
+    i+=2;
+    sleep(5);
+    
   }
 
   return 0;
